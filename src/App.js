b0269a33affd4546edd3539f094601defff34302
Johnson Ojo <32887459+Johnsonojo@users.noreply.github.com>
2020-09-06 13:41:51 +0100

    setup redux toolkit (#3)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Counter from "./pages/Counter";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
